3244a3fd741594881ce2b07f7a07a6dde0372a70
sougetuOte <metral@sougetu.net>
2022-12-07 13:41:46 +0900

    tests

--- hello_flask.py
+++ hello_flask.py
@@ -1,1 +1,13 @@
 from flask import Flask
+
+#Flaskのインスタンスを作成
+app = Flask(__name__)
+
+#ルーティングの指定
+@app.route('/')
+def index():
+    return "Hello World !! ????"
+
+#実行
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
